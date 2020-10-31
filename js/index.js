140e5d04b7b3dbfb8e78f10a6e5a036f56c022c3
Ayush <ayush.kumar20@yahoo.in>
2020-11-01 05:15:20 +0530

    update

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
 function goToHome(){ 
     $('html, body').animate({ scrollTop: $("#background").offset().top }, 1000);
 }
+function goTo(id){
+    $('html, body').animate({ scrollTop: $(id).offset().top }, 1000);
+}

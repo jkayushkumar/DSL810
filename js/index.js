64cdbe4f6a18675a2a370070263216144dabcb41
Ayush <ayush.kumar20@yahoo.in>
2020-11-05 02:09:25 +0530

    Update

--- js/index.js
+++ js/index.js
@@ -13,3 +13,24 @@ window.onclick = function (event) {
         document.getElementById("contactModal").style.display = "none";        
     }
 }
+
+var slideIndex = 1;
+
+function nextAssign(n) {
+    showAssign(slideIndex += n);
+}
+
+function currentAssign(n) {
+    showAssign(slideIndex = n);
+}
+
+function showAssign(n) {
+  var i;
+  var slides = document.getElementsByClassName("innerAssnHolder");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].classList.add("noDisplay");
+  }
+  slides[slideIndex - 1].classList.remove("noDisplay");
+}

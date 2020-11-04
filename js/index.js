59c923f3c6cb7a4af3f929df22fdc2b60f17859d
Ayush <ayush.kumar20@yahoo.in>
2020-11-05 00:16:59 +0530

    update

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
-function goToHome(){ 
+function goToHome() {
     $('html, body').animate({ scrollTop: $("#background").offset().top }, 1000);
 }
-function goTo(id){
+function goTo(id) {
     $('html, body').animate({ scrollTop: $(id).offset().top }, 1000);
+}
+function showContactModal() {
+    document.getElementById("contactModal").style.display = "grid";
+}
+
+window.onclick = function (event) {
+    if (event.target == document.getElementById("contactModal")) {
+        document.getElementById("contactModal").style.display = "none";        
+    }
 }

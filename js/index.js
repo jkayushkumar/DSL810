fb39938298f0a5a5c27d00512749fdab86dd2159
Ayush <ayush.kumar20@yahoo.in>
2021-01-18 00:34:31 +0530

    update

--- js/index.js
+++ js/index.js
@@ -7,10 +7,50 @@ function goTo(id) {
 function showContactModal() {
     document.getElementById("contactModal").style.display = "grid";
 }
+function showDetailsModal(modalNo) {
+    switch (modalNo) {
+        case 1:
+            document.getElementById("assgn1Modal").style.display = "grid";
+            break;
+        case 2:
+            document.getElementById("assgn2Modal").style.display = "grid";
+            break;
+        case 3:
+            document.getElementById("assgn3Modal").style.display = "grid";
+            break;
+        case 4:
+            document.getElementById("assgn4Modal").style.display = "grid";
+            break;
+        case 5:
+            document.getElementById("assgn5Modal").style.display = "grid";
+            break;
+        case 6:
+            document.getElementById("assgn6Modal").style.display = "grid";
+            break;
+        case 7:
+            document.getElementById("assgn7Modal").style.display = "grid";
+            break;
+    }
+}
 
 window.onclick = function (event) {
     if (event.target == document.getElementById("contactModal")) {
-        document.getElementById("contactModal").style.display = "none";        
+        document.getElementById("contactModal").style.display = "none";
+    }
+    if (event.target == document.getElementById("assgn1Modal")) {
+        document.getElementById("assgn1Modal").style.display = "none";
+    }
+    if (event.target == document.getElementById("assgn2Modal")) {
+        document.getElementById("assgn2Modal").style.display = "none";
+    }
+    if (event.target == document.getElementById("assgn3Modal")) {
+        document.getElementById("assgn3Modal").style.display = "none";
+    }
+    if (event.target == document.getElementById("assgn4Modal")) {
+        document.getElementById("assgn4Modal").style.display = "none";
+    }
+    if (event.target == document.getElementById("assgn5Modal")) {
+        document.getElementById("assgn5Modal").style.display = "none";
     }
 }
 
@@ -25,12 +65,16 @@ function currentAssign(n) {
 }
 
 function showAssign(n) {
-  var i;
-  var slides = document.getElementsByClassName("innerAssnHolder");
-  if (n > slides.length) { slideIndex = 1 }
-  if (n < 1) { slideIndex = slides.length }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].classList.add("noDisplay");
-  }
-  slides[slideIndex - 1].classList.remove("noDisplay");
+    var i;
+    var slides = document.getElementsByClassName("innerAssnHolder");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].classList.add("noDisplay");
+    }
+    slides[slideIndex - 1].classList.remove("noDisplay");
+}
+function toggleProject() {
+    document.getElementById("project2").classList.toggle("noDisplay");
+    document.getElementById("project1").classList.toggle("noDisplay");
 }
